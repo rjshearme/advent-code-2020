6771e412938e9f263b01f62cd1b93876b3560be4
rjshearme <rjshearme@gmail.com>
2020-12-06 00:43:25 +0000

    adding in a stupid decorator mad solution for day 4

---
+++ day4/decorator_madness.py
@@ -0,0 +1,83 @@
+import re
+
+
+def validate_byr(func):
+    def wrapper(*args, **kwargs):
+        if re.search(r"\bbyr:(19[2-9]\d|200[012])\b", func(*args, **kwargs)):
+            return func(*args, **kwargs)
+        return ""
+    return wrapper
+
+
+def validate_iyr(func):
+    def wrapper(*args, **kwargs):
+        if re.search(r"\biyr:20(1\d|20)\b", func(*args, **kwargs)):
+            return func(*args, **kwargs)
+        return ""
+    return wrapper
+
+
+def validate_eyr(func):
+    def wrapper(*args, **kwargs):
+        if re.search(r"\beyr:20(2\d|30)\b", func(*args, **kwargs)):
+            return func(*args, **kwargs)
+        return ""
+    return wrapper
+
+
+def validate_hgt(func):
+    def wrapper(*args, **kwargs):
+        if re.search(r"\bhgt:(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)\b", func(*args, **kwargs)):
+            return func(*args, **kwargs)
+        return ""
+    return wrapper
+
+
+def validate_hcl(func):
+    def wrapper(*args, **kwargs):
+        if re.search(r"\bhcl:#[\da-f]{6}\b", func(*args, **kwargs)):
+            return func(*args, **kwargs)
+        return ""
+    return wrapper
+
+
+def validate_ecl(func):
+    def wrapper(*args, **kwargs):
+        if re.search(r"\becl:(amb|blu|brn|gry|grn|hzl|oth)\b", func(*args, **kwargs)):
+            return func(*args, **kwargs)
+        return ""
+    return wrapper
+
+
+def validate_pid(func):
+    def wrapper(*args, **kwargs):
+        if re.search(r"\bpid:\d{9}\b", func(*args, **kwargs)):
+            return func(*args, **kwargs)
+        return ""
+    return wrapper
+
+def count(func):
+    def wrapper(*args, **kwargs):
+        return int(bool(func(*args, **kwargs)))
+    return wrapper
+
+def read(func):
+    def wrapper(*args, **kwargs):
+        return sum(func(a, *args[1:], **kwargs) for a in args[0])
+    return wrapper
+
+@read
+@count
+@validate_iyr
+@validate_byr
+@validate_eyr
+@validate_hgt
+@validate_hcl
+@validate_ecl
+@validate_pid
+def solve(text):
+    return text
+
+with open("input.txt") as file_handle:
+    text = file_handle.read().split("\n\n")
+print(solve(text))
